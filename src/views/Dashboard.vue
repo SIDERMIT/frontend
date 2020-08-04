300e2116c576739733180a477afa5c8320cb7def
Felipinbombin <cephei.1313@gmail.com>
2020-08-03 23:30:15 -0400

    removed template, it was replaced with real optimizations

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -35,56 +35,17 @@
         <template v-else>
             <div class="table">
                 <ul class="optimizations">
-                    <li class="ready">
-                        <div class="icon"><span class="material-icons">check_circle</span></div>
+                    <li v-for="optimization in optimizations" v-bind:key="optimization.network_name" 
+                        v-bind:class="{ ready: optimization.status == 'finished',  error: optimization.status == 'error' }">
+                        <div class="icon"><span class="material-icons">{{ getOptimizationIcon(optimization.status) }}</span></div>
                         <div class="name">
-                            <h4>Network 1</h4>
-                            <span class="p-min">Stage 2 - My City 1</span>
+                            <h4>{{ optimization.network_name }}</h4>
+                            <span class="p-min">{{ optimization.scene_name }} - {{ optimization.city_name }}</span>
                         </div>
-                        <div class="text"><span class="p-min">Result ready</span></div>
+                        <div class="text"><span class="p-min">{{ optimization.status }}</span></div>
                         <a class="btn neuro"><span>View details</span><span class="material-icons">chevron_right</span></a>
                         <a class="btn neuro"><span>View stages</span><span class="material-icons">chevron_right</span></a>
-                    </li>
-                    <li>
-                        <div class="icon"><span class="material-icons">timelapse</span></div>
-                        <div class="name">
-                            <h4>Network 1 copy</h4>
-                            <span class="p-min">Stage 2 - My City 1</span>
-                        </div>
-                        <div class="text"><span class="p-min">Processing...</span></div>
-                        <a class="btn neuro"><span>View details</span><span class="material-icons">chevron_right</span></a>
-                        <a class="btn neuro"><span>View stages</span><span class="material-icons">chevron_right</span></a>
-                    </li>
-                    <li>
-                        <div class="icon"><span class="material-icons">schedule</span></div>
-                        <div class="name">
-                            <h4>Network 2</h4>
-                            <span class="p-min">Stage 2 - My City 1</span>
-                        </div>
-                        <div class="text"><span class="p-min">Enqueued</span></div>
-                        <a class="btn neuro"><span>View details</span><span class="material-icons">chevron_right</span></a>
-                        <a class="btn neuro"><span>View stages</span><span class="material-icons">chevron_right</span></a>
-                    </li>
-                    <li>
-                        <div class="icon"><span class="material-icons">more_horiz</span></div>
-                        <div class="name">
-                            <h4>Network 3</h4>
-                            <span class="p-min">Stage 2 - My City 1</span>
-                        </div>
-                        <div class="text"><span class="p-min">Pending</span></div>
-                        <a class="btn neuro"><span>View details</span><span class="material-icons">chevron_right</span></a>
-                        <a class="btn neuro"><span>View stages</span><span class="material-icons">chevron_right</span></a>
-                    </li>
-                    <li class="error">
-                        <div class="icon"><span class="material-icons">error</span></div>
-                        <div class="name">
-                            <h4>Network 4</h4>
-                            <span class="p-min">Stage 2 - My City 1</span>
-                        </div>
-                        <div class="text"><span class="p-min">With errors</span></div>
-                        <a class="btn neuro"><span>View details</span><span class="material-icons">chevron_right</span></a>
-                        <a class="btn neuro"><span>View stages</span><span class="material-icons">chevron_right</span></a>
-                    </li>
+                    </li>                  
                 </ul>
             </div>
         </template>
@@ -124,6 +85,24 @@ export default {
       init() {
           cities.getAllCities(3).then( response => (this.cities = response.data));
           cities.getRecentOptimizations().then( response => (this.optimizations = response.data));
+      },
+      getOptimizationIcon(status) {
+          let icon = "";
+          switch(status) {
+              case "finished":
+                  icon = "check_circle";
+                  break;
+              case "error":
+                  icon = "error";
+                  break;
+              case "processing":
+                  icon = "timelapse";
+                  break;
+              case "queued":
+                  icon = "schedule";
+                  break;
+          }
+          return icon
       }
   }, 
   mounted() {
