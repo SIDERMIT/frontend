f324780efc07bb49eb9fe0c5f87fb43bd2b2ca75
Felipinbombin <cephei.1313@gmail.com>
2020-08-05 02:42:06 -0400

    added new view to create scene

---
+++ src/views/NewScene.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="NewScene">
+    <div class="header">          
+        <h1>Add new scene</h1>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NewScene',
+  components: {
+    
+  }
+}
+</script>
