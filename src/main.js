8dbc16e56106c3930e9fc3ae2f698173a4f9042f
Felipinbombin <cephei.1313@gmail.com>
2020-08-07 13:10:43 -0400

    added echart as dependency

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import ECharts from 'vue-echarts'
 import VueBreadcrumbs from 'vue-2-breadcrumbs'
 
 require('@/assets/css/base.css')
 require('@/assets/css/style.css')
 
+Vue.component('v-chart', ECharts)
 Vue.use(VueBreadcrumbs);
 
 Vue.config.productionTip = false
