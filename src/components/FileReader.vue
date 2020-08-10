94d32e8621975637787dc3a0f802a047196748cd
Felipinbombin <cephei.1313@gmail.com>
2020-08-10 12:33:17 -0400

    added FileReder component to read files locally

---
+++ src/components/FileReader.vue
@@ -0,0 +1,24 @@
+<template>
+  <label class="btn">
+    <div class="icon"><span class="material-icons">publish</span></div> Choose file
+    <input type="file" @change="loadTextFromFile">
+  </label>
+</template>
+
+<script>
+export default {
+  methods: {
+    loadTextFromFile(ev) {
+      const file = ev.target.files[0];
+      const reader = new FileReader();
+
+      reader.onload = e => this.$emit("load", e.target.result);
+      reader.readAsText(file);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
