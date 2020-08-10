9f913b4df10a765a3deaf8964bf88e784dab3b0c
Felipinbombin <cephei.1313@gmail.com>
2020-08-10 13:22:07 -0400

    renamed close button with ok name

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -10,7 +10,7 @@
         <div class="modal-base" v-if="showBase">
             <slot name="base">
               <button class="btn solo red" @click="$emit('cancel')" v-if="showCancelButton">Cancel</button>
-              <button class="btn solo" @click="$emit('close');$emit('ok')"><slot name="close-button-name">Close</slot></button>
+              <button class="btn solo" @click="$emit('close');$emit('ok')"><slot name="close-button-name">Ok</slot></button>
             </slot>
         </div>
     </div>
