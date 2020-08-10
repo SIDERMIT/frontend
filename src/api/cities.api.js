fc50cd28f418091d31a5f4a9ebc60d1a21d6ef30
Felipinbombin <cephei.1313@gmail.com>
2020-08-10 00:44:50 -0400

    added call to get pajek file content from parameters

--- src/api/cities.api.js
+++ src/api/cities.api.js
@@ -1,14 +1,15 @@
 import httpClient from './httpClient';
 
 const BASE_END_POINT = '/api'
-const END_POINT = `${BASE_END_POINT}/cities`;
+const END_POINT = `${BASE_END_POINT}/cities/`;
 
 const getAllCities = (limit) => httpClient.get(END_POINT, {limit});
-const getCity = (publicId) => httpClient.get(`${END_POINT}/${publicId}`);
-const deleteCity = (publicId) => httpClient.delete(`${END_POINT}/${publicId}`);
-const updateCity = (publicId) => httpClient.patch(`${END_POINT}/${publicId}`);
-const createCity = () => httpClient.post(END_POINT, {});
-const duplicateCity = (publicId) => httpClient.post(`${END_POINT}/${publicId}/duplicate`);
+const getCity = (publicId) => httpClient.get(`${END_POINT}${publicId}`);
+const deleteCity = (publicId) => httpClient.delete(`${END_POINT}${publicId}`);
+const updateCity = (publicId) => httpClient.patch(`${END_POINT}${publicId}`);
+const createCity = (name, n, p, l, g, graph) => httpClient.post(END_POINT, {name, n, p, l, g, graph});
+const duplicateCity = (publicId) => httpClient.post(`${END_POINT}${publicId}/duplicate`);
+const getPajekFile = (n,p,l,g) => httpClient.get(`${END_POINT}build_graph_file`, {params:{n,p,l,g}});
 
 const getRecentOptimizations = () => httpClient.get(`${BASE_END_POINT}/recent_optimizations`, {});
 
@@ -20,4 +21,5 @@ export default {
     deleteCity,
     getRecentOptimizations,
     duplicateCity,
+    getPajekFile
 }
