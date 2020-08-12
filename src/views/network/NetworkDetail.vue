bd43d8c0ab86e53200d7311cb76c9c8b5dfae67a
Felipinbombin <cephei.1313@gmail.com>
2020-08-12 01:57:50 -0400

    added logic to load transport network data

--- src/views/network/NetworkDetail.vue
+++ src/views/network/NetworkDetail.vue
@@ -8,10 +8,27 @@
 </template>
 
 <script>
+import transportNetworksAPI from '@/api/transportNetworks.api';
+
 export default {
   name: 'NetworkDetail',
   components: {
     
+  }, 
+  methods: {
+    setData(transportNetwork) {
+      this.transportNetwork = transportNetwork;
+    },
+  },
+  beforeRouteEnter (to, from, next) {
+    transportNetworksAPI.getTransportNetwork(to.params.transportNetworkPublicId).then(response => (next(vm => vm.setData(response.data))));
+  },
+  beforeRouteUpdate(to, from, next) {
+    this.city = {};
+    transportNetworksAPI.getTransportNetwork(to.params.transportNetworkPublicId).then(response => {
+      this.setData(response.data); 
+      next();
+    });
   }
 }
 </script>
