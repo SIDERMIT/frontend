9cb0d1c75754f767a112652ff5f8ce20119541ef
Felipinbombin <cephei.1313@gmail.com>
2020-08-18 18:10:51 -0400

    createScene receives data object

--- src/api/scenes.api.js
+++ src/api/scenes.api.js
@@ -6,7 +6,7 @@ const END_POINT = `${BASE_END_POINT}/scenes/`;
 const getScene = (publicId) => httpClient.get(`${END_POINT}${publicId}`);
 const deleteScene = (publicId) => httpClient.delete(`${END_POINT}${publicId}`);
 const updateScene = (publicId) => httpClient.patch(`${END_POINT}${publicId}`);
-const createScene = (name, n, p, l, g, graph) => httpClient.post(END_POINT, {name, n, p, l, g, graph});
+const createScene = (data) => httpClient.post(END_POINT, data);
 const duplicateScene = (publicId) => httpClient.post(`${END_POINT}${publicId}/duplicate/`);
 
 export default {
