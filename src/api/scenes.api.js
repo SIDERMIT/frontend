032f4b7e43e573e77506756f05db101419129188
Felipinbombin <cephei.1313@gmail.com>
2020-09-24 11:53:37 -0300

    fixed url syntax, avoid 301 code

--- src/api/scenes.api.js
+++ src/api/scenes.api.js
@@ -3,7 +3,7 @@ import httpClient from './httpClient';
 const BASE_END_POINT = '/api'
 const END_POINT = `${BASE_END_POINT}/scenes/`;
 
-const getScene = (publicId) => httpClient.get(`${END_POINT}${publicId}`);
+const getScene = (publicId) => httpClient.get(`${END_POINT}${publicId}/`);
 const deleteScene = (publicId) => httpClient.delete(`${END_POINT}${publicId}`);
 const updateScene = (publicId, data) => httpClient.put(`${END_POINT}${publicId}`, data);
 const createScene = (data) => httpClient.post(END_POINT, data);
