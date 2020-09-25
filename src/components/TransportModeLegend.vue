73c081b6022f2c68583f131f530114a702867186
Gabriel Segura <gabe0005@gmail.com>
2020-09-25 04:07:47 -0300

    leyends

--- src/components/TransportModeLegend.vue
+++ src/components/TransportModeLegend.vue
@@ -1,8 +1,77 @@
 <template>
   <div class="TransportModeLegend">
     <Modal v-if="show" @close="$emit('close')" :showBase="false">
-          <template slot="title">transport mode legend</template>
-          <template slot="content">transport mode content</template>
+          <template slot="title">
+                <h2>Terminology</h2>
+            </template>
+            <template slot="content">
+                <div class="table">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <th><span>Symbol</span></th>
+                                <th><span>Unit</span></th>
+                                <th><span>Description</span></th>
+                            </tr>
+                            <tr>
+                                <td><span>𝐶𝑜</span></td>
+                                <td>[$/h − 𝑣𝑒h]</td>
+                                <td><span>Unitary cost per vehicle per period of time</span></td>
+                            </tr>
+                            <tr>
+                                <td><span>𝐶1</span></td>
+                                <td>[$/h − 𝑝𝑎𝑥]</td>
+                                <td><span>Unitary cost per seat per period of time</span></td>
+                            </tr>
+                            <tr>
+                                <td><span>𝐶2</span></td>
+                                <td>[$/h − 𝑘𝑚]</td>
+                                <td><span>Unitary cost per km per period of time</span></td>
+                            </tr>
+                            <tr>
+                                <td><span>𝜈</span></td>
+                                <td>[𝑘𝑚/h]</td>
+                                <td><span>Cruise speed</span></td>
+                            </tr>
+                            <tr>
+                                <td><span>B&A</span></td>
+                                <td>Sequential</td>
+                                <td><span>Boarding and alighting way</span></td>
+                            </tr>
+                            <tr>
+                                <td><span>𝑡</span></td>
+                                <td>[𝑠/𝑝𝑎𝑥]</td>
+                                <td><span>Boarding and alighting time</span></td>
+                            </tr>
+                            <tr>
+                                <td><span>𝑓𝑚𝑎𝑥</span></td>
+                                <td>[𝑣𝑒h/h]</td>
+                                <td><span>Maximum frequency</span></td>
+                            </tr>
+                            <tr>
+                                <td><span>𝐾𝑚𝑎𝑥</span></td>
+                                <td>[𝑝𝑎𝑥/𝑣𝑒h]</td>
+                                <td><span>Maximum vehicle size</span></td>
+                            </tr>
+                            <tr>
+                                <td><span>𝜃</span></td>
+                                <td>&nbsp;</td>
+                                <td><span>Regularity arrivals</span></td>
+                            </tr>
+                            <tr>
+                                <td><span>𝑡𝑎𝑡</span></td>
+                                <td>[𝑚𝑖𝑛]</td>
+                                <td><span>Technological access time</span></td>
+                            </tr>
+                            <tr>
+                                <td><span>𝐷</span></td>
+                                <td>&nbsp;</td>
+                                <td><span>Parallel lines</span></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </template>
     </Modal>
   </div>
 </template>
--- src/components/UserLegend.vue
+++ src/components/UserLegend.vue
@@ -1,8 +1,67 @@
 <template>
   <div class="UserLegend">
     <Modal v-if="show" @close="$emit('close')" :showBase="false">
-          <template slot="title">user legend</template>
-          <template slot="content">user content</template>
+          <template slot="title">
+                <h2>Terminology</h2>
+            </template>
+            <template slot="content">
+                <div class="table">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <th><span>Symbol</span></th>
+                                <th><span>Unit</span></th>
+                                <th><span>Description</span></th>
+                            </tr>
+                            <tr>
+                                <td><span>𝑣𝑎</span></td>
+                                <td>[𝑘𝑚/h]</td>
+                                <td><span>Walking speed</span></td>
+                            </tr>
+                            <tr>
+                                <td><span>𝑝𝑣</span></td>
+                                <td>[𝑈𝑆$/h]</td>
+                                <td><span>Value of in-vehicle time savings</span></td>
+                            </tr>
+                            <tr>
+                                <td><span>𝑝𝑤</span></td>
+                                <td>[𝑈𝑆$/h]</td>
+                                <td><span>Value of waiting time savings</span></td>
+                            </tr>
+                            <tr>
+                                <td><span>𝑝𝑎</span></td>
+                                <td>[𝑈𝑆$/h]</td>
+                                <td><span>Value of access time savings</span></td>
+                            </tr>
+                            <tr>
+                                <td><span>𝑝𝑡</span></td>
+                                <td>[𝐸𝐼𝑉]</td>
+                                <td><span>Pure transfer penalty</span></td>
+                            </tr>
+                            <tr>
+                                <td><span>𝑠𝑝𝑣</span></td>
+                                <td>[𝑈𝑆$/h]</td>
+                                <td><span>Subjective value of in- vehicle time savings</span></td>
+                            </tr>
+                            <tr>
+                                <td><span>𝑠𝑝𝑤</span></td>
+                                <td>[𝑈𝑆$/h]</td>
+                                <td><span>Subjective value of waiting time savings</span></td>
+                            </tr>
+                            <tr>
+                                <td><span>𝑠𝑝𝑎</span></td>
+                                <td>[𝑈𝑆$/h]</td>
+                                <td><span>Subjective value of access time savings</span></td>
+                            </tr>
+                            <tr>
+                                <td><span>𝑠𝑝𝑡</span></td>
+                                <td>[𝐸𝐼𝑉]</td>
+                                <td><span>Subjective pure transfer penalty</span></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </template>
     </Modal>
   </div>
 </template>
