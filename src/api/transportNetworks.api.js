c0e332c034e86237bf74e8813ac7b0482ac680ae
Felipinbombin <cephei.1313@gmail.com>
2020-09-29 18:28:01 -0300

    added endpoint to create default routes

--- src/api/transportNetworks.api.js
+++ src/api/transportNetworks.api.js
@@ -3,16 +3,18 @@ import httpClient from './httpClient';
 const BASE_END_POINT = '/api'
 const END_POINT = `${BASE_END_POINT}/transport_networks/`;
 
-const getTransportNetwork = (publicId) => httpClient.get(`${END_POINT}${publicId}`);
+const getTransportNetwork = (publicId) => httpClient.get(`${END_POINT}${publicId}/`);
 const deleteTransportNetwork = (publicId) => httpClient.delete(`${END_POINT}${publicId}`);
 const updateTransportNetwork = (publicId) => httpClient.patch(`${END_POINT}${publicId}`);
 const createTransportNetwork = (name, n, p, l, g, graph) => httpClient.post(END_POINT, {name, n, p, l, g, graph});
 const duplicateTransportNetwork = (publicId) => httpClient.post(`${END_POINT}${publicId}/duplicate/`);
+const createDefaultRoutes = (scene_public_id, default_routes) => httpClient.post(`${END_POINT}create_default_routes/`, {scene_public_id, default_routes});
 
 export default {
     getTransportNetwork,
     deleteTransportNetwork,
     updateTransportNetwork,
     createTransportNetwork,
-    duplicateTransportNetwork
+    duplicateTransportNetwork,
+    createDefaultRoutes
 }
