2194cbd33fc2cee2685eb3bee6d90303245e2b8f
Felipinbombin <cephei.1313@gmail.com>
2020-09-30 18:06:25 -0300

    fixed url to create and update transport networks

--- src/api/transportNetworks.api.js
+++ src/api/transportNetworks.api.js
@@ -5,8 +5,8 @@ const END_POINT = `${BASE_END_POINT}/transport_networks/`;
 
 const getTransportNetwork = (publicId) => httpClient.get(`${END_POINT}${publicId}/`);
 const deleteTransportNetwork = (publicId) => httpClient.delete(`${END_POINT}${publicId}`);
-const updateTransportNetwork = (publicId) => httpClient.patch(`${END_POINT}${publicId}`);
-const createTransportNetwork = (name, n, p, l, g, graph) => httpClient.post(END_POINT, {name, n, p, l, g, graph});
+const updateTransportNetwork = (publicId, name, route_set) => httpClient.patch(`${END_POINT}${publicId}/`, {name, route_set});
+const createTransportNetwork = (scene_public_id, name, route_set) => httpClient.post(END_POINT, {scene_public_id, name, route_set});
 const duplicateTransportNetwork = (publicId) => httpClient.post(`${END_POINT}${publicId}/duplicate/`);
 const createDefaultRoutes = (scene_public_id, default_routes) => httpClient.post(`${END_POINT}create_default_routes/`, {scene_public_id, default_routes});
 
